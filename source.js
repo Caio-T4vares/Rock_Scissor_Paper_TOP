48e1adca0d0d43b80582e65f6e376fe1bb92a7e2
Caio <102376017+Caio-T4vares@users.noreply.github.com>
2023-05-26 19:55:05 -0300

    Js part finished

--- source.js
+++ source.js
@@ -4,24 +4,53 @@ function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
   computerSelection = computerSelection.toLowerCase();
 
-  if (playerSelection === computerSelection) return "DRAW!";
+  if (playerSelection === computerSelection) {
+    console.log("Draw!");
+    return 3;
+  }
   if (playerSelection === "rock") {
     if (computerSelection === "paper") {
-      return "You lost! Paper beats Rock!";
+      console.log("You lost! Paper beats Rock!");
+      return 0;
     } else if (computerSelection === "scissor") {
-      return "You win! Rock beats Scissor!";
+      console.log("You win! Rock beats Scissor!");
+      return 1;
     }
   } else if (playerSelection === "scissor") {
     if (computerSelection === "rock") {
-      return "You lost! Rock beats Scissor";
+      console.log("You lost! Rock beats Scissor");
+      return 0;
     } else if (computerSelection === "paper") {
-      return "You win! Scissor beats Paper";
+      console.log("You win! Scissor beats Paper");
+      return 1;
     }
   } else if (playerSelection === "paper") {
     if (computerSelection === "scissor") {
-      return "You lost! Scissor beats Paper";
+      console.log("You lost! Scissor beats Paper");
+      return 0;
     } else if (computerSelection === "rock") {
-      return "You win! Paper beats Rock";
+      console.log("You win! Paper beats Rock");
+      return 1;
     }
   } else return "Invalid input";
+}
+
+function game() {
+  let playerWins = 0;
+  let computerWins = 0;
+  for (let cont = 0; cont < 5; cont++) {
+    console.log(`Round ${cont + 1}`);
+    playerSelection = prompt(`Choice your "weapon" :O`);
+    computerSelection = getComputerChoice();
+    let result = playRound(playerSelection, computerSelection);
+    if (result === 1) {
+      playerWins++;
+    } else if (result === 0) {
+      computerWins++;
+    }
+  }
+  if (playerWins > computerWins)
+    console.log("Congratulations, you are the winner!");
+  else if (playerWins < computerWins) console.log("OH NO! You lost the game!");
+  else console.log("Draw!!");
 }
